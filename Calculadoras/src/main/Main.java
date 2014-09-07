0b7e9f62deb73c495113ea0bffccdf73ca2b72fd
Guilherme Soster <guilhermesosters@gmail.com>
2014-09-07 17:00:39 -0300

    termindo ..

--- Calculadoras/src/main/Main.java
+++ Calculadoras/src/main/Main.java
@@ -9,9 +9,9 @@ public class Main {
 
 	public static void main(String args[]){		
 		Calc calc = new Calc();//canto superior esquerdo
-		//Calc1 calc1 = new Calc1();//centro superior
-		//Calc2 calc2 = new Calc2();//centro da tela
-		//CalcSoma calcSoma = new CalcSoma();//inferior perto do meio
+		Calc1 calc1 = new Calc1();//centro superior
+		Calc2 calc2 = new Calc2();//centro da tela
+		CalcSoma calcSoma = new CalcSoma();//inferior perto do meio
 	}
 	
 }
